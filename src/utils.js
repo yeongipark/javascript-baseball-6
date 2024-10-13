6d9d9591a3e0fcbf44d58d3a1f45bb44290ba3c5
Yeongi Park <yeongi0111@naver.com>
2024-10-13 22:49:22 +0900

    DELETE : 불필요한 파일 삭제

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-
-export function printMessage(msg) {
-  MissionUtils.Console.print(msg);
-}
-
-export function errorMessage(msg) {
-  throw new Error(msg);
-}
