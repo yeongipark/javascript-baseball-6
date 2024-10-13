9c1634129a1055ccc756375ec38875a288e8b06d
Yeongi Park <yeongi0111@naver.com>
2024-10-13 21:27:02 +0900

    FEAT : InputView 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+import { message } from '../constant.js';
+
+export default class InputView {
+  static async inputNumbers() {
+    return await Console.readLineAsync(message.INPUT_MESSAGE);
+  }
+
+  static async inputRestartNumber() {
+    return await Console.readLineAsync(message.RESTART_MESSAGE + '\n');
+  }
+}
